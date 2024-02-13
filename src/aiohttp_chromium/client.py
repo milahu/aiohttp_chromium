435c307b1d4b6ebc6ea8a68758b7fb5eb87f61ec
Milan Hauth <milahu@gmail.com>
2024-02-13 20:24:06 +0100

    use Fetch.requestPaused

--- src/aiohttp_chromium/client.py
+++ src/aiohttp_chromium/client.py
@@ -1352,6 +1352,188 @@ class ClientSession(aiohttp.ClientSession):
         loader_id_by_frame_id = dict()
         loader_id_by_guid = dict()
 
+        use_requestPaused = True
+
+        # handle the Fetch.requestPaused event
+
+        async def requestPaused(args):
+            request_id = args.get("networkId")
+            #logger.debug(f"requestPaused {request_id} {json.dumps(args, indent=2)}")
+            #interception_id = args.get("requestId")
+            #resource_type = args.get("resourceType") # "Document" or ...
+            #frame_id = args.get("frameId")
+
+            url = args["request"]["url"]
+            responseStatusCode = args.get("responseStatusCode")
+            logger.debug(f"requestPaused {request_id} status {responseStatusCode} from url {url}")
+            _args = {"requestId": args['requestId']}
+            if responseStatusCode in [301, 302, 303, 307, 308]:
+                logger.debug(f"requestPaused {request_id} handling redirect")
+                return await target.execute_cdp_cmd("Fetch.continueResponse", _args)
+
+            # fix: Can only get response body on requests captured after headers received.
+            # fix: The request must be paused in the HeadersReceived stage
+            if not "responseHeaders" in args:
+                # request is before the "HeadersReceived" stage
+                logger.debug(f"requestPaused {request_id} headers not received -> Fetch.continueResponse")
+                return await target.execute_cdp_cmd("Fetch.continueResponse", _args)
+
+            # headers received. now we can get the response body
+            logger.debug(f"requestPaused {request_id} headers received -> getting the response body")
+            #logger.debug(f"requestPaused {request_id} {json.dumps(args, indent=2)}")
+
+            # no. this requires interceptionId
+            #stream = await driver.execute_cdp_cmd("Network.takeResponseBodyForInterceptionAsStream", _args)
+
+            # no: websockets.exceptions.ConnectionClosedError
+            #stream = await target.execute_cdp_cmd("Fetch.takeResponseBodyAsStream", _args)
+            # fix: target -> driver
+            stream = await driver.execute_cdp_cmd("Fetch.takeResponseBodyAsStream", _args)
+
+            stream_handle = stream["stream"]
+            # stream_handle is just a number
+            #logger.debug(f"requestPaused {request_id} stream", stream)
+
+            # TODO implement aiohttp.streams.StreamReader
+            # dont store the whole response in memory
+            # return a reader, and the consumer calls reader.iter_any()
+            # to read chunks of the response until EOF
+            # async with get_reader() as reader:
+            #     async for slice in reader.iter_any():
+            #         print(slice)
+            data_eof = False
+            body = None
+            while data_eof == False:
+                _args = {
+                    "handle": stream_handle,
+                    # error: Read offset is specificed for a stream that does not support random access
+                    #"offset": idx,
+                    #"size": 100,
+                }
+                data = await target.execute_cdp_cmd("IO.read", _args)
+                # the EOF event is passed in a separate read with data == ""
+                data_eof = data["eof"]
+                # base64Encoded
+                data = base64.b64decode(data["data"]) if data["base64Encoded"] else data["data"]
+                # data can be str or bytes
+                if body == None:
+                    if type(data) == str:
+                        body = ""
+                    else:
+                        body = b""
+                # empty data is always passed as empty string
+                # fix: TypeError: can't concat str to bytes
+                if len(data) > 0:
+                    logger.debug(f"requestPaused {request_id} data {len(data)} {repr(data[:100])}")
+                    body += data
+                if data_eof:
+                    logger.debug(f"requestPaused {request_id} eof")
+
+            # no: Unable to continue request as is after body is taken
+            """
+            _args = {"requestId": args['requestId']}
+            await target.execute_cdp_cmd("Fetch.continueResponse", _args)
+            return
+            """
+
+            # test: size limit of body
+            # https://groups.google.com/g/chrome-debugging-protocol/c/w65z0cMqgvc
+            # all response overrides are inherently limited by whatever fits in a single CDP message.
+            # this seems to be limited only by RAM, and by CDP timeouts
+            """
+            #body = os.urandom(10_000)
+            body = b"0" * 1_000_000_000
+            if False:
+                body = b""
+                for i in range(1000):
+                    body += b"0" * 1_000
+                    body += (f" len = {i*1_000:e} ").encode("ascii")
+            for header in args["responseHeaders"]:
+                if header["name"] == "Content-Type":
+                    header["value"] = "application/octet-stream; charset=binary"
+                if header["name"] == "Content-Length":
+                    header["value"] = str(len(body))
+            args["responseHeaders"].append({
+                "name": "Content-Disposition",
+                "value": 'attachment; filename="blob.bin"',
+            })
+            """
+
+            # return empty response
+            # turn file download into empty page
+            # no. file downloads should not leave the current page
+            # instead of Fetch.fulfillRequest, use Fetch.failRequest
+            """
+            requestId = args["requestId"]
+            body = (f"ok request {requestId}").encode("utf8")
+            for header in args["responseHeaders"]:
+                if header["name"] == "Content-Type":
+                    header["value"] = "text/plain; charset=utf8"
+                if header["name"] == "Content-Length":
+                    header["value"] = str(len(body))
+                if header["name"] == "Content-Disposition":
+                    header["value"] = "inline"
+            """
+
+            # dont send response to chromium
+            """
+            _args = {
+                "requestId": args['requestId'],
+                # Failed, Aborted, TimedOut, AccessDenied,
+                # ConnectionClosed, ConnectionReset, ConnectionRefused, ConnectionAborted, ConnectionFailed,
+                # NameNotResolved, InternetDisconnected, AddressUnreachable, BlockedByClient, BlockedByResponse
+                "errorReason": "Aborted",
+            }
+            await target.execute_cdp_cmd("Fetch.failRequest", _args)
+            return
+            """
+
+            # we dont need the raw body any more. encode it to base64
+            if type(body) == str:
+                body = body.encode("utf8")
+            # fix: TypeError: Object of type bytes is not JSON serializable
+            body = base64.b64encode(body).decode("ascii")
+            _args = {
+                "requestId": args['requestId'],
+                "responseCode": args["responseStatusCode"],
+                # fix: Please unblock challenges.cloudflare.com to proceed.
+                "responseHeaders": args["responseHeaders"],
+                "body": body,
+            }
+            if args["responseStatusText"] != "":
+                # empty string throws "Invalid http status code or phrase"
+                _args["responsePhrase"] = args["responseStatusText"]
+
+            # FIXME stream
+            # how does this work with an infinite-size body?
+            # Fetch.continueResponse accepts no data
+            # https://groups.google.com/g/chrome-debugging-protocol/c/w65z0cMqgvc
+            # Unfortunately, there's no streaming support for Fetch network interception at the moment,
+            # so all response overrides are inherently limited by whatever fits in a single CDP message.
+            await target.execute_cdp_cmd("Fetch.fulfillRequest", _args)
+
+            # ok, this works, but only for small non-stream responses
+            """
+            body = await target.execute_cdp_cmd("Fetch.getResponseBody", _args, timeout=1)
+            body = base64.b64decode(body["body"]) if body["base64Encoded"] else body["body"]
+            logger.debug(f"requestPaused {request_id} body", body[:100])
+
+            # ?
+            # no: This check is taking longer than expected. Check your Internet connection and refresh the page if the issue persists.
+            #return await target.execute_cdp_cmd("Fetch.continueResponse", _args)
+
+            body = base64.b64encode(body).decode("ascii")
+            _args = {
+                "requestId": args['requestId'],
+                "responseCode": args["responseStatusCode"],
+                # fix: Please unblock challenges.cloudflare.com to proceed.
+                "responseHeaders": args["responseHeaders"],
+                "body": body,
+            }
+            await target.execute_cdp_cmd("Fetch.fulfillRequest", _args)
+            """
+
+        # TODO? remove in favor of "async def requestPaused"
         async def requestWillBeSent(args):
             nonlocal expected_request_id
             request_url = args["request"]["url"]
@@ -1361,6 +1543,11 @@ class ClientSession(aiohttp.ClientSession):
 
             #logger.debug(f"req {request_id_path} requestWillBeSent {json.dumps(args, indent=2)}")
             #logger.debug(f"req {request_id_path} requestWillBeSent url {request_url}")
+
+            if use_requestPaused:
+                logger.debug(f"requestWillBeSent {request_id} {request_url}")
+                #logger.debug(f"requestWillBeSent {request_id} {json.dumps(args, indent=2)}")
+                return
 
             # note: request_url can change due to protocol upgrade from http to https
             request_url_by_id[request_id] = request_url
@@ -1401,6 +1588,12 @@ class ClientSession(aiohttp.ClientSession):
             #logger.debug(f"req {request_id_path} requestWillBeSentExtraInfo " + json.dumps(args, indent=2))
             loader_id = loader_id_by_request_id[request_id]
             request_id_path = f"{loader_id}/{request_id}" if loader_id else request_id
+
+            if use_requestPaused:
+                #logger.debug(f"requestWillBeSentExtraInfo {request_id}")
+                logger.debug(f"requestWillBeSentExtraInfo {request_id} {json.dumps(args, indent=2)}")
+                return
+
             request_url = request_url_by_id[request_id]
             #logger.debug(f"req {request_id_path} requestWillBeSentExtraInfo {json.dumps(args, indent=2)}")
 
@@ -1442,6 +1635,11 @@ class ClientSession(aiohttp.ClientSession):
         async def responseReceived(args):
 
             nonlocal done_responseReceived
+
+            if use_requestPaused:
+                request_id = args["requestId"]
+                logger.debug(f"responseReceived {request_id}")
+                return
 
             request_id = args["requestId"]
             #logger.debug(f"req {request_id} responseReceived {json.dumps(args, indent=2)}")
@@ -1680,6 +1878,13 @@ class ClientSession(aiohttp.ClientSession):
             request_id = args.get("requestId")
             #logger.debug(f"req {request_id_path} loadingFinished " + str(args.get("requestId")))
             #logger.debug(f"req {request_id_path} loadingFinished {json.dumps(args, indent=2)}")
+
+            if use_requestPaused:
+                logger.debug(f"loadingFinished " + str(args.get("requestId")))
+                return
+
+            #logger.debug("loadingFinished " + str(args.get("requestId")))
+            #logger.debug(f"loadingFinished {json.dumps(args, indent=2)}")
             if request_id != expected_request_id:
                 return
             event_loadingFinished.set()
@@ -1695,6 +1900,10 @@ class ClientSession(aiohttp.ClientSession):
             request_id_path = f"{loader_id}/{request_id}" if loader_id else request_id
 
             #logger.debug(f"req {request_id_path} downloadWillBegin {json.dumps(args, indent=2)}")
+
+            if use_requestPaused:
+                logger.debug(f"downloadWillBegin")
+                return
 
             # response_data was set by responseReceived
             # in most cases, responseReceived is called before downloadWillBegin
@@ -1851,6 +2060,33 @@ class ClientSession(aiohttp.ClientSession):
 
         target = await driver.current_target
         #logger.debug(f"target.id {target.id}")
+
+
+
+        # enable the Fetch.requestPaused event
+
+        args = {
+            # by default, requestStage == "Request"
+            "patterns": [
+                {"requestStage": "Request", "urlPattern":"*"},
+                {"requestStage": "Response", "urlPattern":"*"},
+            ]
+        }
+
+        # only for driver.current_target
+        # FIXME target == None
+        # target = ...
+        await target.execute_cdp_cmd("Fetch.enable", args)
+        await target.add_cdp_listener("Fetch.requestPaused", requestPaused)
+
+        """
+        # for all targets
+        # TODO verify
+        await driver.execute_cdp_cmd("Fetch.enable", args)
+        await driver.add_cdp_listener("Fetch.requestPaused", requestPaused)
+        """
+
+
 
         # TODO? add cdp listeners only once
         # once per session?
